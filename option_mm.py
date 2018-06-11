8376518d3234ade90b95bdba392807ab0bf7e4b0
Xintong Wang <t-xiwa@microsoft.com>
2018-06-11 13:03:56 -0400

    add loss

--- option_mm.py
+++ option_mm.py
@@ -1,21 +1,20 @@
 import pdb
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import norm
+from scipy.stats import norm, entropy
 from scipy.misc import derivative
 
 def u(w):
 	return np.log(w)
 
 r, mu, sigma, s0 = 0.08, 0.15, 0.3, 50
-# one year span
 st = np.random.lognormal(np.log(s0)+(mu-sigma*sigma/2), sigma, 10000)
 fl = np.floor(np.min(st)/5)*5
 cl = np.ceil(np.max(st)/5)*5
 hist, bin_edges = np.histogram(st, bins = np.linspace(fl, cl, (cl-fl)/5+1))
-#plt.hist(st, bins = np.linspace(fl, cl, (cl-fl)/5+1))
+plt.hist(st, bins = np.linspace(fl, cl, (cl-fl)/5+1))
 #plt.show()
-# strike prices considered
+
 K = bin_edges[:-1]
 call = []
 put = []
@@ -33,9 +32,8 @@ for k in K:
 
 w = [5000]*K.shape[0]
 subjective = [1.0/K.shape[0]]*K.shape[0]
-
 it = 0
-while True:
+while it <= 1000:
 	call_mm = []
 	put_mm = []
 	p_i = []
@@ -51,6 +49,13 @@ while True:
 	for i in range(0, K.shape[0]):
 		p_i.append(1.0/K.shape[0]*dw[i]/deno)
 
+	#KL divergence
+	loss = entropy(1.0*hist/sum(hist), p_i)
+	if it%50==0:
+		print('The constant utility is {}'.format(sum(p_i*np.array(u(w)))))
+		print('For iteration {}, the loss is {}'.format(it, loss))
+		print(p_i)
+		
 	for i in range(0, K.shape[0]):
 		call_mm.append(np.exp(-r)*sum(np.maximum(K-K[i], 0)*p_i))
 		put_mm.append(np.exp(-r)*sum(np.maximum(K[i]-K, 0)*p_i))
@@ -66,11 +71,6 @@ while True:
 	for i in range(0, K.shape[0]):
 		put_quant = put_quant + np.maximum(K[i]-K, 0)*np.sign(put_buy_sell[i])
 
-	w = w + call_mm*np.sign(call_buy_sell) + put_mm*np.sign(put_buy_sell)-call_quant-put_quant
+	w = w + call_mm*np.sign(call_buy_sell) + put_mm*np.sign(put_buy_sell)-\
+		call_quant-put_quant
 	it = it+1
-	if it%100==0:
-		print(it)
-		print(p_i)
-		print(1.0*hist/sum(hist))
-	#pdb.set_trace()
-#pdb.set_trace()

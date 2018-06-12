ef7fbedb8d82838f91335acece7055c1ea92c336
Xintong Wang <t-xiwa@microsoft.com>
2018-06-12 16:42:37 -0400

    minor changes

--- option_mm.py
+++ option_mm.py
@@ -8,7 +8,8 @@ def u(w):
 	return np.log(w)
 
 def v(w):
-	return -np.exp(-0.1*w)
+	#return -np.exp(-0.1*w)
+	return 1.0/w
 
 r, mu, sigma, s0 = 0.08, 0.15, 0.3, 50
 np.random.seed(101)
@@ -16,7 +17,7 @@ st = np.random.lognormal(np.log(s0)+(mu-sigma*sigma/2), sigma, 10000)
 fl = np.floor(np.min(st)/5)*5
 cl = np.ceil(np.max(st)/5)*5
 hist, bin_edges = np.histogram(st, bins = np.linspace(fl, cl, (cl-fl)/5+1))
-plt.hist(st, bins = np.linspace(fl, cl, (cl-fl)/5+1))
+#plt.hist(st, bins = np.linspace(fl, cl, (cl-fl)/5+1))
 #plt.show()
 
 K = bin_edges[:-1]
@@ -37,6 +38,7 @@ for k in K:
 w = [10000]*K.shape[0]
 subjective = [1.0/K.shape[0]]*K.shape[0]
 it = 0
+
 while it <= 2000:
 	call_mm = []
 	put_mm = []
@@ -45,20 +47,11 @@ while it <= 2000:
 	dw = []
 	for i in range(0, K.shape[0]):
 		dw.append(derivative(u, w[i], dx=1e-6))
-
-	deno = 0
-	for i in range(0, K.shape[0]):
-		deno = deno + 1.0/K.shape[0] * dw[i]
-
-	for i in range(0, K.shape[0]):
-		p_i.append(1.0/K.shape[0]*dw[i]/deno)
+	deno = sum(1.0/K.shape[0]*np.array(dw))
+	p_i = 1.0/K.shape[0]*np.array(dw)/deno
 
 	#KL divergence
 	loss = entropy(1.0*hist/sum(hist), p_i)
-	if it%50==0:
-		print('The constant utility is {}'.format(sum(p_i*np.array(u(w)))))
-		print('For iteration {}, the loss is {}'.format(it, loss))
-		print(p_i)
 		
 	for i in range(0, K.shape[0]):
 		call_mm.append(np.exp(-r)*sum(np.maximum(K-K[i], 0)*p_i))
@@ -75,9 +68,19 @@ while it <= 2000:
 	for i in range(0, K.shape[0]):
 		put_quant = put_quant + np.maximum(K[i]-K, 0)*np.sign(put_buy_sell[i])
 
+	if it%50==0:
+		print('The constant utility is {}'.format(sum(p_i*np.array(u(w)))))
+		print('For iteration {}, the loss is {}'.format(it, loss))
+		print(p_i)
+		# print(call_quant+put_quant)
+		# print(w)
+
+	# TO-DO: cost should be the implicit solution of the constant utility function
 	w = w + call_mm*np.sign(call_buy_sell) + put_mm*np.sign(put_buy_sell)-\
 		call_quant-put_quant
 	it = it+1
 np.set_printoptions(suppress=True, formatter={'float_kind':'{:0.4f}'.format})
-print('The sampled distribution is')
-print(1.0*hist/sum(hist))
+plt.plot(K, 1.0*hist/sum(hist), 'ro')
+plt.plot(K, p_i, 'bx')
+plt.show()
+# pdb.set_trace()
